9df3e2dbde199256745c8c5cb5366209e7c0fdfa
Ashley Alfred <ashley@sagerecruiting.me>
2020-05-15 19:58:53 -0400

    Update RockPaperScissors.c

--- RockPaperScissors.c
+++ RockPaperScissors.c
@@ -3,70 +3,112 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define USER 0
+#define AI 1
+
+void single_game(int num_hands);
+int single_hand(void);
+int winner(int userchoice, int aichoice);
+
+
 int main(void)
 {
-    
     // Initialize random number generator
     srand(time(NULL));
-    int n;
-    int x = 0;
-    int Rock = 1; 
-    int Paper = 2; 
-    int Scissors = 3; 
-    
-    do 
+
+    do
     {
-        printf ("Your turn! \n");
-        do
+        single_game(3);
+    }
+    while(get_char("Would you like to play again? Y/N \n") == 'Y');
+}
+
+
+void single_game(int num_hands)   //best out of num_hands
+{
+    int userwins = 0; //counters to store the number
+    int aiwins = 0;   //of hands a user won
+    for (int i = 0; i < num_hands; i++)
+    {
+        //calling the function (three times). Calling the function runs the function!
+        if (single_hand() == USER) //if game "n" = 0, the userwins and add one to userwins. Else, add one to aiwins. repeat "n" times.
         {
-            n = get_int ("Rock = 1, Paper = 2, Scissors = 3. Please enter a number to play: \n");
-        }
-        while (n < 1 || n > 3);
-    
-        if (n == 1)
-        {
-            printf ("You picked Rock. Now, it's my turn! \n");
-        }
-        else if (n == 2)
-        {
-            printf ("You picked Paper. Now, it's my turn! \n");
+            userwins++;
         }
         else
         {
-            printf ("You picked Scissors. Now, it's my turn! \n");
+            aiwins++;
         }
-    
-        printf ("Rolling the dice .... \n");
-    
-        int a = rand() % 3 + 1;
-
-        if (a == 1)
-        {
-            printf ("I picked Rock. \n");
-        }
-        else if (a == 2)
-        {
-            printf ("I picked Paper. \n");
-        }
-        else if (a == 3)
-        {
-            printf ("I picked Scissors. \n");
-        }
-        
-
-        if ((n == Rock && a == Scissors) || (n == Paper && a == Rock) || (n == Scissors && a == Paper))
-        {
-            printf ("You Won! \n");
-        }
-    
-       if ((a == Rock && n == Scissors) || (a == Paper && n == Rock) || (a == Scissors && n == Paper))
-        {
-            printf ("Better luck next time! \n"); 
-        }
-        
-        x++;
     }
-    while (x < 3);
-    
+    if (userwins > aiwins)
+    {
+        printf ("You Won! \n");
+    }
+    else
+    {
+        printf("I won ... yes! Better luck next time! \n");
+    }
+}
+
+//functions outside of main:
+
+int single_hand(void)
+{
+    int userchoice;
+    do
+    {
+        userchoice = get_int("Rock = 1, Paper = 2, Scissors = 3. Please enter a number to play: \n");
+    }
+    while (userchoice < 1 || userchoice > 3);
+
+    if (userchoice == 1)
+    {
+        printf ("You picked Rock.\n");
+    }
+    else if (userchoice == 2)
+    {
+        printf ("You picked Paper.\n");
+    }
+    else
+    {
+        printf ("You picked Scissors.\n");
+    }
+
+    printf("Now, it's my turn!\n");
+    printf ("Rolling the dice .... \n");
+
+    int aichoice = rand() % 3 + 1;
+
+    if (aichoice == 1)
+    {
+        printf ("I picked Rock. \n");
+    }
+    else if (aichoice == 2)
+    {
+        printf ("I picked Paper. \n");
+    }
+    else if (aichoice == 3)
+    {
+        printf ("I picked Scissors. \n");
+    }
+    return winner(userchoice, aichoice);
+}
+
+int winner(int userchoice, int aichoice)
+{
+    const int Rock = 1;
+    const int Paper = 2;
+    const int Scissors = 3;
+
+    if ((userchoice == Rock && aichoice == Scissors) || (userchoice == Paper && aichoice == Rock) || (userchoice == Scissors && aichoice == Paper))
+    {
+        printf ("You Won best out of 3! \n");
+        return USER;
+    }
+    else
+    {
+        printf ("I won best out of 3, better luck next time! \n");
+        return AI;
+    }
 
 }
